532576b8cd01b881318e12601a4b90d8a6af4c2b
Kamo-Git-Hub <53765597+Kamo-Git-Hub@users.noreply.github.com>
2020-03-06 19:04:40 -0800

    Updates

---
+++ precache-manifest.65f9fe5c0741dcfd20a70486b4500d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6731db0d964f85ef4e11694bc61bbf81",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "a41719d6dbc1402809c4",
+    "url": "/slider/static/css/main.d358704c.chunk.css"
+  },
+  {
+    "revision": "50e40eb0e0f291f2f659",
+    "url": "/slider/static/js/2.c705578b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/slider/static/js/2.c705578b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41719d6dbc1402809c4",
+    "url": "/slider/static/js/main.3ad960fc.chunk.js"
+  },
+  {
+    "revision": "9b8d4e560076c5ec6cf5",
+    "url": "/slider/static/js/runtime-main.b1310a78.js"
+  }
+]);
